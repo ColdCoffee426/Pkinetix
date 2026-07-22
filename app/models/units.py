2d530ca654079469c2be8b1529ac3656d5083b3b
Qasim <ReiAmane@MacBook-Pro.local>
2026-07-22 15:47:51 +0500

    addition of units for results

--- app/models/units.py
+++ app/models/units.py
@@ -1,11 +1,17 @@
 from dataclasses import dataclass
 
+
 @dataclass(slots=True)
 class Units:
     """
-    Units used throughout a pharmacokinetic project.
+    Stores measurement units used by a project.
     """
+
     time: str = "h"
     concentration: str = "ng/mL"
     dose: str = "mg"
     body_weight: str = "kg"
+    volume: str = "L"
+    clearance: str = "L/h"
+    auc: str = "ng·h/mL"
+    aumc: str = "ng·h²/mL"
